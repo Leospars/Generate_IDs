983c06a27d15ebb829b168f3cda346a2ae7be2c7
Leon Gayle <103550846+Leospars@users.noreply.github.com>
2024-08-04 07:48:26 -0500

    create ID textbox on layout successful

--- app.py
+++ app.py
@@ -12,9 +12,16 @@ from generateID_ui import Ui_MainWindow
 from PyQt5.QtCore import Qt, QMimeData, QRect, QSize, QPoint
 from PyQt5.QtGui import QDrag, QPixmap, QFont, QPainter
 from PyQt5.QtWidgets import QFileDialog, QApplication, QLabel, QMainWindow
-from PyQt5.uic import loadUi
+
+# from PyQt5.uic import loadUi
 
 textBoxNum = 0
+click_flag = 0
+
+
+def increment_click():
+    global click_flag
+    click_flag += 1
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -25,28 +32,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setEventHandlers()
 
     def addWidgets(self):
-        self.template_overlay = Canvas(self.template_img)
-        self.template_overlay.setGeometry(self.template_img.geometry())
-        self.template_overlay.draw = lambda: self.addPaint(self.template_overlay)
-        self.horizontalLayout.addWidget(self.template_overlay)
-
-    def addPaint(self, canvas):
-        painter = canvas.painter
-        # tint background
-        painter.setBrush(Qt.black)
-        painter.setOpacity(0.5)
-        painter.drawRect(canvas.rect())
-        painter.setBrush(Qt.blue)
-        painter.setOpacity(0.3)
-        painter.drawRect(canvas.geometry())
-        print("Canvas rect:", canvas.rect(), "Canvas geometry:", canvas.geometry(),
-              "Template img rect:", self.template_img.rect(), "Template img geometry:", self.template_img.geometry())
+        print("tempate image parent: ", self.template_img.parent())
+        self.canvas = Canvas(self.template_img.parent())
+        self.canvas.setGeometry(self.template_img.geometry())
+        self.canvas.draw = lambda: self.addPaint(self.canvas)
 
     def setEventHandlers(self):
         self.uploadButton.clicked.connect(self.uploadImage)
         self.generateIDButton.clicked.connect(self.generateID)
+        self.mousePressEvent = lambda e: increment_click()
         self.addTextBoxButton.clicked.connect(self.addTextBox)
-
+        self.clearIDButton.clicked.connect(self.canvas.clearCanvas)
         self.txtBox = Canvas(self)
 
     def uploadImage(self):
@@ -56,33 +52,43 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.template_img.setPixmap(QPixmap(template_filename))
             print(template_filename)
 
+    def addPaint(self, canvas):
+        painter = canvas.painter
+        # tint background
+        painter.setBrush(Qt.green)
+        painter.setOpacity(0.2)
+        painter.drawRect(canvas.geometry())
+        print("Canvas rect:", canvas.rect(), "Canvas geometry:", canvas.geometry(),
+              "Template img rect:", self.template_img.rect(), "Template img geometry:", self.template_img.geometry())
+
     def generateID(self):
         print("Generate ID")
 
     def addTextBox(self):
         # Change template image cursor
-        self.template_img.setCursor(Qt.CrossCursor)
-        self.template_overlay.mousePressEvent = lambda event: self.createTextBoxOnClick(event)
-        print("Add Teox")
+        self.canvas.setCursor(Qt.CrossCursor)
+        self.canvas.mousePressEvent = lambda event: self.createTextBoxOnClick(event)
+        print("Add Textbox")
 
     def createTextBoxOnClick(self, event):
         # get values from user input
-        print(f"Width: {self.txtBoxW.placeholderText()}, Height: {self.txtBoxH.text()}")
-        width = self.txtBoxW.text() | self.txtBoxW.placeholderText()
-        height = self.txtBoxH.text() | self.txtBoxH.placeholderText()
+        width = self.txtBoxW.text() if self.txtBoxW.text() else self.txtBoxW.placeholderText()
+        height = self.txtBoxH.text() if self.txtBoxH.text() else self.txtBoxH.placeholderText()
         width = int(width)
         height = int(height)
         font = self.fontComboBox.currentFont()  # get font from combobox
 
         #increment textBoxID
-        global textBoxNum
-        textBoxNum += 1
+        _id = self.idTypeName.text() if self.idTypeName.text() else self.idTypeName.placeholderText()
+        if _id in self.canvas.rectIDs:
+            _id += "_" + str(len(self.canvas.rectIDs))
 
-        self.template_overlay.addTextBoxContext(
+        self.canvas.addTextBoxContext(
             rects=[QRect(event.pos().x(), event.pos().y(), width, height)],
-            rectIDs=[f"TextBox{textBoxNum}"],
+            rectIDs=[_id],
             rectFonts=[font]
         )
+        self.canvas.mousePressEvent = lambda ev: None
         print(f"TextBox{textBoxNum} added at {event.pos().x(), event.pos().y()} with size {width}x{height}")
 
 if __name__ == '__main__':
