72d6b55e8fe8a201d425e82f76756ebe83514017
Leon Gayle <103550846+Leospars@users.noreply.github.com>
2024-08-05 10:29:06 -0500

    Create functions to generate toolbox requesting ID field's data

--- generateTabs.py
+++ generateTabs.py
@@ -1,11 +1,10 @@
-import PyQt5
 from PIL.ImageFont import FreeTypeFont
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import QRect
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QTabWidget, QWidget, QToolBox, QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QToolBox
 
-from tests.SpecialComboBox import TtfComboBox
+from tests.SpecialComboBox import TtfComboBox, FontSizeComboBox
 from tests.canvas import Canvas
 
 class DataObject:
@@ -13,8 +12,7 @@ class DataObject:
 	to put in the data_type use type(var).__name__
 	"""
 
-	def __init__(self, data_type: str, data: str, position: QRect, metadata: QFont | FreeTypeFont,
-				 alignment="Center"):
+	def __init__(self, data_type: str, data: str, position: QRect, metadata: QFont | FreeTypeFont, alignment="Center"):
 		self.type = data_type.__name__ if isinstance(data_type, type) else data_type
 		self.data = data
 		self.position = position
@@ -25,114 +23,100 @@ def get_data_from_canvas(canvas: Canvas):
 	data_array = []
 	for rect, label, font in zip(canvas.rects, canvas.rectLabels, canvas.rectFonts):
 		canvas_data = DataObject(type(label), label, rect, font)
-		print(canvas_data)
 		data_array.append(canvas_data)
 	return data_array
 
-def generateDataTab(MainWindow: QMainWindow, canvas: Canvas):
-	"""
-	Set up the user interface for the MainWindow.
-	Args:
-		MainWindow (QMainWindow): The main window object.
-		canvas (Canvas): The canvas object.
-	"""
-	MainWindow.setObjectName("MainWindow")
-	MainWindow.setWindowTitle("dataUpdate")
-	MainWindow.resize(768, 411)
-	m_wind = MainWindow
+# helper function to translate texts in the window
+__translate__ = QtCore.QCoreApplication.translate
+
+def generateToolBox(parent: QToolBox, canvas: Canvas | list[DataObject]):
+	# if data types entered do not match expected throw an error
+	if not isinstance(canvas, Canvas) and not isinstance(canvas, list):
+		raise TypeError("canvas must be of type Canvas")
+
+	if not isinstance(parent, QToolBox):
+		raise TypeError("parent must be of type QToolBox")
+
+	tool_box = QtWidgets.QToolBox()
+	tool_box.setObjectName("toolBox")
+
+	# get canvas data for generating frames
 	canvas_data = get_data_from_canvas(canvas)
 
-	# Create the central widget and set its object name
-	m_wind.Tab = QTabWidget()
-	m_wind.Tab = QtWidgets.QTabWidget(MainWindow)
-	m_wind.tool_box = QToolBox()  # initialize toolbox on page
-
+	# create a page for each field in the toolbox
 	for box in canvas_data:
 		field_name = box.data
 		font = box.metadata
+		data_type = type(font).__name__
 
 		# create a page for each field in the toolbox
-		field_page = m_wind[field_name] = QWidget()
-		field_page.setObjectName(field_name)
-		field_page.setGeometry(0, 0, 1100, 300)
+		page = QtWidgets.QWidget(tool_box)
+		page.setObjectName(field_name + '_page')
+		page.setGeometry(0, 0, 730, 232)
 
 		# initialize font combobox
-		ttfComboBox = m_wind[field_name + '_ttfBox'] = TtfComboBox()
-		ttfComboBox.setGeometry(QtCore.QRect(0, 0, 226, 22))
-		ttfComboBox.setObjectName(f"{field_name}_ttfBox")
+		ttf_combo_box = TtfComboBox(page)
+		ttf_combo_box.setGeometry(QtCore.QRect(0, 0, 226, 22))
+		ttf_combo_box.setObjectName(field_name + '_ttfBox')
+		ttf_combo_box.setCurrentFont(font)
 
-		# initialize text box for user to enter comma seperated data
-		# if data should be an image it should be a path to the images folder to be implemented
-		data_input = m_wind[field_name + '_data']
-		data_input = PyQt5.QtWidgets.QPlainTextEdit(field_page)
-		data_input.setObjectName(field_name + '_data')
-		data_input.setGeometry(0, 30, 800, 80)
-		data_input.placeholderText("Enter comma seperated data here...")
+		# initialize font size combobox
+		font_size_combo_box = FontSizeComboBox(page)
+		font_size_combo_box.setGeometry(QtCore.QRect(246, 0, 226, 22))
+		font_size_combo_box.setObjectName(field_name + '_fontSizeBox')
+		font_size_combo_box.setCurrentText(str(font.pointSize()))
+		font_size_combo_box.setGeometry(QtCore.QRect(240, 0, 73, 22))
+		font_size_combo_box.setMaxVisibleItems(8)
 
-		# add the font combobox and text box to the field page
-		field_page.layout().addWidget(ttfComboBox)
-		field_page.layout().addWidget(data_input)
-		m_wind.tool_box.addItem(field_page, "")
+		# initialize text box
+		text_box = QtWidgets.QPlainTextEdit(page)
+		text_box.setGeometry(QtCore.QRect(0, 30, 800, 80))
+		text_box.setObjectName(field_name + '_data')
+		if data_type == "QFont":
+			text_box.setPlaceholderText(__translate__("MainWindow", "Enter comma seperated values here . . .\n"))
+		else:
+			# TODO: Add support for auto generating images
+			text_box.setPlaceholderText(__translate__("MainWindow", "Enter path to folder with images"))
 
-# MainWindow.dataTab = QtWidgets.QWidget()
-# MainWindow.dataTab.setObjectName("dataTab")
-# MainWindow.verticalLayout_4 = QtWidgets.QVBoxLayout(MainWindow.dataTab)
-# MainWindow.verticalLayout_4.setObjectName("verticalLayout_4")
-#
-# MainWindow.toolBox = QtWidgets.QToolBox(MainWindow.dataTab)
-# MainWindow.toolBox.setObjectName("toolBox")
-# MainWindow.toolBoxPage1 = QtWidgets.QWidget()
-# MainWindow.toolBoxPage1.setGeometry(QtCore.QRect(0, 0, 1108, 609))
-# MainWindow.toolBoxPage1.setObjectName("toolBoxPage1")
-# MainWindow.textBox1_Data = QtWidgets.QPlainTextEdit(MainWindow.toolBoxPage1)
-# MainWindow.textBox1_Data.setGeometry(QtCore.QRect(0, 30, 800, 80))
-# sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-# sizePolicy.setHorizontalStretch(0)
-# sizePolicy.setVerticalStretch(0)
-# sizePolicy.setHeightForWidth(MainWindow.textBox1_Data.sizePolicy().hasHeightForWidth())
-# MainWindow.textBox1_Data.setSizePolicy(sizePolicy)
-# MainWindow.textBox1_Data.setObjectName("textBox1_Data")
-# MainWindow.idTtfComboBox_1 = QtWidgets.QFontComboBox(MainWindow.toolBoxPage1)
-# MainWindow.idTtfComboBox_1.setGeometry(QtCore.QRect(0, 0, 226, 22))
-# MainWindow.idTtfComboBox_1.setObjectName("idTtfComboBox_1")
-# MainWindow.toolBox.addItem(MainWindow.toolBoxPage1, "")
-# MainWindow.toolBoxPage2 = QtWidgets.QWidget()
-# MainWindow.toolBoxPage2.setGeometry(QtCore.QRect(0, 0, 1090, 243))
-# MainWindow.toolBoxPage2.setObjectName("toolBoxPage2")
-# MainWindow.textBox2_Data = QtWidgets.QPlainTextEdit(MainWindow.toolBoxPage2)
-# MainWindow.textBox2_Data.setGeometry(QtCore.QRect(0, 30, 800, 80))
-# MainWindow.textBox2_Data.setObjectName("textBox2_Data")
-# MainWindow.idTtfComboBox_2 = QtWidgets.QFontComboBox(MainWindow.toolBoxPage2)
-# MainWindow.idTtfComboBox_2.setGeometry(QtCore.QRect(0, 0, 226, 22))
-# MainWindow.idTtfComboBox_2.setObjectName("idTtfComboBox_2")
-# MainWindow.toolBox.addItem(MainWindow.toolBoxPage2, "")
-# MainWindow.page = QtWidgets.QWidget()
-# MainWindow.page.setGeometry(QtCore.QRect(0, 0, 1108, 609))
-# MainWindow.page.setObjectName("page")
-# MainWindow.toolBox.addItem(MainWindow.page, "")
-# MainWindow.verticalLayout_4.addWidget(MainWindow.toolBox)
-# MainWindow.Tab.addTab(MainWindow.dataTab, "")
+		# add the combo boxes to the page
+		tool_box.addItem(page, __translate__("MainWindow", field_name))
+		vLayout.addWidget(tool_box)  # Add the toolbox to the vLayout in the window
 
-def dataTabUpdate(self, canvas: Canvas):
-	pass
+	tool_box.show()
+	QtCore.QMetaObject.connectSlotsByName(parent)
+
+def updateToolBox(tool_box: QToolBox, canvas: Canvas):
+	# ignore up to last page than add to toolbox
+	last_page = tool_box.count()
+	canvas_data = get_data_from_canvas(canvas)
+	canvas_data = canvas_data[last_page:]
+	generateToolBox(tool_box, canvas_data)
 
 class MainWindow(QMainWindow):
 	def __init__(self):
 		super(QMainWindow, self).__init__()
-		generateDataTab(self, canvas)
+		generateToolBox(self, canvas)
 
 if __name__ == "__main__":
-	# test the generateDataTab function
+	# test the generateTabs function
 	app = QApplication([])
 	canvas = Canvas()
 	canvas.setGeometry(0, 0, 1100, 300)
-	canvas.addTextBoxContent(
-		[QRect(0, 0, 100, 100), QRect(0, 0, 100, 100)],
-		["Name", "ID_Number"],
-		[QFont("Arial", 12), QFont("Lucida Console", 12)]
-	)
+	canvas.addTextBoxContent([QRect(0, 0, 100, 100), QRect(0, 0, 100, 100)], ["Name", "ID_Number"],
+							 [QFont("Arial", 12), QFont("Lucida Console", 12)])
+
 	window = QMainWindow()
-	generateDataTab(window, canvas)
+	window.resize(800, 600)
+	window.setWindowTitle("Generate Toolbox for IDs")
+	centralWidget = QtWidgets.QWidget()
+	window.setCentralWidget(centralWidget)
+	vLayout = QtWidgets.QVBoxLayout(centralWidget)
+	vLayout.setObjectName("verticalLayout")
+
+	window.tool_box = QtWidgets.QToolBox()
+	vLayout.addWidget(window.tool_box)
+
+	generateToolBox(window.tool_box, canvas)
 	window.show()
 	app.exec()
 
