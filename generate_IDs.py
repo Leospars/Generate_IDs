a75eb2d9afc5a9019548d7db200c6374ba463b15
Leon Gayle <103550846+Leospars@users.noreply.github.com>
2024-08-05 10:07:25 -0500

    Refactored and Updated IDE Reformat Settings for All Files

--- generate_IDs.py
+++ generate_IDs.py
@@ -1,6 +1,6 @@
 import os
-
 import time
+
 from PIL import Image, ImageDraw
 from PIL import ImageFont
 
@@ -9,107 +9,105 @@ cls()
 
 # All students list
 # Create fake names of charcters from movies and cartoons
-lvl1_students = [ "Sherice Marytal", "Mirabell Madrigal", "Jazmine Topples", "Mary Poppins", "Kim Possible",
-                  "Ron Stoppable", "Kermit the Frog", "Danny Phantom", "Mickey Mouse"]
+lvl1_students = ["Sherice Marytal", "Mirabell Madrigal", "Jazmine Topples", "Mary Poppins", "Kim Possible",
+				 "Ron Stoppable", "Kermit the Frog", "Danny Phantom", "Mickey Mouse"]
 
 # Create fake names from anime characters
 lvl2_students = ["Eren Yeager", "Armin Arlert", "Edward Elric", "Riza Hawkeye", "Killua Zoldyck", "Gon Freecss",
-                 "Phosphophyllite", "Shinobu Kocho", "Senku Ishigami", "Shikamaru Nara", "Rika Furude", "Yami Sukehiro",
-                 "Seras Victoria", "Gintoki Sakata", "Misa Amane", "L Lawliet", "Fuhrer King Bradley", "Shinra Kusakabe",
-                 "Ritsu Kageyama", "Hinata Shoyo"]
+				 "Phosphophyllite", "Shinobu Kocho", "Senku Ishigami", "Shikamaru Nara", "Rika Furude",
+				 "Yami Sukehiro", "Seras Victoria", "Gintoki Sakata", "Misa Amane", "L Lawliet", "Fuhrer King Bradley",
+				 "Shinra Kusakabe", "Ritsu Kageyama", "Hinata Shoyo"]
 
 # declare variable for handling paths
 abspath = lambda path: os.path.abspath(path)
 relpath = lambda path: os.path.relpath(path)
 
-
 def generate_cert(
-        student_list,
-        save_folder="",
-        template="L1 Blank Certificate.png",
-        cfont=ImageFont.truetype("./font/Sans.ttf", 55),
-        align="center",
+		data_list,
+		save_folder="",
+		template="L1 Blank Certificate.png",
+		cfont=ImageFont.truetype("./font/Sans.ttf", 55),
+		align="center",
 ):
-    if save_folder == "":
-        # Get folder location
-        import tkinter as tk
-        from tkinter import filedialog
+	if save_folder == "":
+		# Get folder location
+		import tkinter as tk
+		from tkinter import filedialog
 
-        tk = tk.Tk()
-        tk.withdraw()
+		tk = tk.Tk()
+		tk.withdraw()
 
-        save_folder = filedialog.askdirectory(
-            title="Save Generated Certificates to ", initialdir=os.getcwd()
-        )
+		save_folder = filedialog.askdirectory(
+			title="Save Generated Certificates to ", initialdir=os.getcwd()
+		)
 
-        print(f"Save folder: {save_folder}")
-        tk.destroy()
+		print(f"Save folder: {save_folder}")
+		tk.destroy()
 
-    if save_folder == "":
-        print("No folder selected. Exiting...")
-        return
+	if save_folder == "":
+		print("No folder selected. Exiting...")
+		return
 
-    if template == "":
-        # Get folder location
-        import tkinter as tk
-        from tkinter import filedialog
+	if template == "":
+		# Get folder location
+		import tkinter as tk
+		from tkinter import filedialog
 
-        tk = tk.Tk()
-        tk.withdraw()
+		tk = tk.Tk()
+		tk.withdraw()
 
-        template = tk.filedialog.askopenfile(title="Template file")
-        print(f"template location : {template}")
-        tk.destroy()
+		template = tk.filedialog.askopenfile(title="Template file")
+		print(f"template location : {template}")
+		tk.destroy()
 
-    filename = ""
-    for name in student_list:
-        # Open an Template Image
-        img = Image.open(template)
+	filename = ""
+	for name in data_list:
+		# Open an Template Image
+		img = Image.open(template)
 
-        # Call draw Method to add 2D graphics in an image
-        draw = ImageDraw.Draw(img)
+		# Call draw Method to add 2D graphics in an image
+		draw = ImageDraw.Draw(img)
 
-        # Scale the location based on image built open of size (1037, 801)
-        (imgsz_x, imgsz_y) = img.size
-        scale_x = imgsz_x / 1037
-        scale_y = imgsz_y / 801
+		# Scale the location based on image built open of size (1037, 801)
+		(imgsz_x, imgsz_y) = img.size
+		scale_x = imgsz_x / 1037
+		scale_y = imgsz_y / 801
 
-        # draw a text box transparent background
-        rect = [(230 * scale_x, 330 * scale_y), (992 * scale_x, 407 * scale_y)]
-        # draw.rectangle(rect, fill=None, outline=2)
-        width = rect[1][0] - rect[0][0]
-        height = rect[1][1] - rect[0][1]
-        x = rect[0][0]
-        y = rect[0][1]
+		# draw a text box transparent background
+		rect = [(230 * scale_x, 330 * scale_y), (992 * scale_x, 407 * scale_y)]
+		# draw.rectangle(rect, fill=None, outline=2)
+		width = rect[1][0] - rect[0][0]
+		height = rect[1][1] - rect[0][1]
+		x = rect[0][0]
+		y = rect[0][1]
 
-        # Evaluate position of text in textbox
-        if align == "center":
-            text_x = (x + width / 2) - draw.textlength(name, font=cfont) / 2
-            text_y = (y + height / 2) - draw.textlength("W", font=cfont) / 2
+		# Evaluate position of text in textbox
+		if align == "center":
+			text_x = (x + width / 2) - draw.textlength(name, font=cfont) / 2
+			text_y = (y + height / 2) - draw.textlength("W", font=cfont) / 2
 
-        # Add Text to an image
-        draw.text((text_x, text_y), name, font=cfont, fill=(0, 0, 0), align="center")
+		# Add Text to an image
+		draw.text((text_x, text_y), name, font=cfont, fill=(0, 0, 0), align="center")
 
-        # Save the edited image
-        if not os.path.exists(save_folder):
-            os.makedirs(save_folder)
+		# Save the edited image
+		if not os.path.exists(save_folder):
+			os.makedirs(save_folder)
 
-        # Change name characters or pattern to meet structure nsfor file names
-        restrict_char = "`'’"
-        print(f"name: {name}")
-        for char in restrict_char:
-            name = name.replace(char, "_")
+		# Change name characters or pattern to meet structure nsfor file names
+		restrict_char = "`'’"
+		print(f"name: {name}")
+		for char in restrict_char:
+			name = name.replace(char, "_")
 
-        filename = name + " Certificate.png"
-        save_path = abspath(os.path.join(save_folder, filename))
-        img.save(save_path)
+		filename = name + " Certificate.png"
+		save_path = abspath(os.path.join(save_folder, filename))
+		img.save(save_path)
 
-    print(f"Completed generating {len(lvl1_students)} certificates for {save_folder}")
-    # delay one second then open folder
-    time.sleep(1)
-    os.startfile(os.path.join(save_folder, filename))
-
+	print(f"Completed generating {len(lvl1_students)} certificates for {save_folder}")
+	# delay one second then open folder
+	time.sleep(1)
+	os.startfile(os.path.join(save_folder, filename))
 
 # Generate Certificates for Level 1 Students
-generate_cert(lvl1_students, template="MGI_Blank Lvl1.png", cfont=ImageFont.truetype("./font/Sans.ttf", 115))
-generate_cert(lvl2_students, template="MGI_Blank Lvl2.png", cfont=ImageFont.truetype("./font/Sans.ttf", 115))
+generate_cert(lvl1_students, template="img/MGI_Blank Lvl1.png", cfont=ImageFont.truetype("./font/Sans.ttf", 115))
+generate_cert(lvl2_students, template="img/MGI_Blank Lvl2.png", cfont=ImageFont.truetype("./font/Sans.ttf", 115))
--- tests/GetFonts.py
+++ tests/GetFonts.py
@@ -2,110 +2,106 @@ import os
 
 import PyQt5.QtGui
 from PIL import ImageFont, ImageDraw, Image
-from PyQt5.QtWidgets import QApplication
-
 
 class GetFonts:
-    def __init__(self):
-        self.system_ttf_fonts = self.find_all_ttf_fonts()  # List of all ttf fonts files in the system
-        self.font_dirmap = self.get_font_dirmap()  # Map all ttf font family to font file location
+	def __init__(self):
+		self.system_ttf_fonts = self.find_all_ttf_fonts()  # List of all ttf fonts files in the system
+		self.font_dirmap = self.get_font_dirmap()  # Map all ttf font family to font file location
 
-    @staticmethod
-    def __get_fonts_directory__():
-        if os.name == "nt":
-            return os.path.join(os.environ['WINDIR'], 'Fonts')
-        elif os.name == "posix":
-            if 'darwin' in os.uname().sysname.lower():
-                return "/System/Library/Fonts"
-            else:
-                return "/usr/share/fonts"
-        else:
-            return None
+	@staticmethod
+	def __get_fonts_directory__():
+		if os.name == "nt":
+			return os.path.join(os.environ['WINDIR'], 'Fonts')
+		elif os.name == "posix":
+			if 'darwin' in os.uname().sysname.lower():
+				return "/System/Library/Fonts"
+			else:
+				return "/usr/share/fonts"
+		else:
+			return None
 
-    @staticmethod
-    def __get_downloads_directory__():
-        if os.name == "nt":
-            return os.path.join(os.environ['USERPROFILE'], 'Downloads')
-        elif os.name == "posix":
-            return os.path.join(os.environ['HOME'], 'Downloads')
-        else:
-            return None
+	@staticmethod
+	def __get_downloads_directory__():
+		if os.name == "nt":
+			return os.path.join(os.environ['USERPROFILE'], 'Downloads')
+		elif os.name == "posix":
+			return os.path.join(os.environ['HOME'], 'Downloads')
+		else:
+			return None
 
-    @staticmethod
-    def find_all_ttf_fonts():
-        fonts_dir = GetFonts.__get_fonts_directory__()
-        if not fonts_dir:
-            print("This function is only for Windows, Linux, and macOS.")
-            return None
+	@staticmethod
+	def find_all_ttf_fonts():
+		fonts_dir = GetFonts.__get_fonts_directory__()
+		if not fonts_dir:
+			print("This function is only for Windows, Linux, and macOS.")
+			return None
 
-        ttf_fonts = []
+		def get_ttf_fonts(_dir):
+			fonts = []
+			for root, dirs, files in os.walk(_dir):
+				for file in files:
+					if file.endswith(".ttf"):
+						fonts.append(os.path.join(root, file))
+			return fonts
 
-        def get_ttf_fonts(_dir):
-            fonts = []
-            for root, dirs, files in os.walk(_dir):
-                for file in files:
-                    if file.endswith(".ttf"):
-                        fonts.append(os.path.join(root, file))
-            return fonts
+		directories = [fonts_dir, os.path.abspath("./font"),
+					   os.path.join(GetFonts.__get_downloads_directory__(), "Fonts")]
+		system_ttf_fonts = []
+		for _dir in directories:
+			if os.path.exists(_dir):
+				ttf_fonts = get_ttf_fonts(_dir)
+				# Check if all the fonts can be loaded by PIL
+				for font_location in ttf_fonts:
+					try:
+						loaded_font = ImageFont.truetype(font_location)
+						system_ttf_fonts.append(font_location)
+					except Exception as e:
+						print(f"Cannot load {font_location} font")
+						ttf_fonts.remove(font_location)
+						continue
 
-        directories = [fonts_dir, os.path.abspath("./font"), os.path.join(GetFonts.__get_downloads_directory__(), "Fonts")]
-        system_ttf_fonts = []
-        for _dir in directories:
-            if os.path.exists(_dir):
-                ttf_fonts = get_ttf_fonts(_dir)
-                # Check if all the fonts can be loaded by PIL
-                for font_location in ttf_fonts:
-                    try:
-                        loaded_font = ImageFont.truetype(font_location)
-                        system_ttf_fonts.append(font_location)
-                    except Exception as e:
-                        print(f"Cannot load {font_location} font")
-                        ttf_fonts.remove(font_location)
-                        continue
+		return system_ttf_fonts  # List of all ttf fonts in the system
 
-        return system_ttf_fonts  # List of all ttf fonts in the system
+	@staticmethod
+	def get_font_dirmap():
+		font_dirmap = {}
+		all_fonts = GetFonts.find_all_ttf_fonts()
+		for font_location in all_fonts:
+			try:
+				loaded_font = ImageFont.truetype(font_location)
+				font_family = loaded_font.getname()[0]
+				font_dirmap[font_family] = font_location
+			except Exception as e:
+				print(f"Cannot load {font_location} font")
+				continue
+		font_dirmap = dict(sorted(font_dirmap.items()))  # Sort the dictionary by font family
+		return font_dirmap
 
-    @staticmethod
-    def get_font_dirmap() :
-        font_dirmap = {}
-        all_fonts = GetFonts.find_all_ttf_fonts()
-        for font_location in all_fonts:
-            try:
-                loaded_font = ImageFont.truetype(font_location)
-                font_family = loaded_font.getname()[0]
-                font_dirmap[font_family] = font_location
-            except Exception as e:
-                print(f"Cannot load {font_location} font")
-                continue
-        font_dirmap = dict(sorted(font_dirmap.items())) # Sort the dictionary by font family
-        return font_dirmap
+	# Search for the font file in the fonts direct
+	def find_ttf_font(self, font_family):
+		return self.font_dirmap.get(font_family, None)
 
-    # Search for the font file in the fonts direct
-    def find_ttf_font(self, font_family):
-        return self.font_dirmap.get(font_family, None)
-
-
-    def qFontToPILFont(self, qFont: PyQt5.QtGui.QFont):
-        if qFont.family() in self.font_dirmap:
-            return ImageFont.truetype(self.font_dirmap[qFont.family()], qFont.pointSize())
-        else:
-            return None
+	def qFontToPILFont(self, qFont: PyQt5.QtGui.QFont):
+		if qFont.family() in self.font_dirmap:
+			return ImageFont.truetype(self.font_dirmap[qFont.family()], qFont.pointSize())
+		else:
+			return None
 
 if __name__ == "__main__":
-    font = PyQt5.QtGui.QFont("Lucida Console", 12)
-    print(f"Font: {font.family()}, {font.pointSize()}pt")
-    gf = GetFonts()
+	font = PyQt5.QtGui.QFont("Lucida Console", 12)
+	print(f"Font: {font.family()}, {font.pointSize()}pt")
+	gf = GetFonts()
 
-    print(f"Font Family: {font.family()}")
-    font_path = gf.find_ttf_font(font.family())
-    if font_path:
-        print(f"Font path: {font_path}")
-    else:
-        print(f"Font '{font.family()}' not found in the fonts directory. ")
+	print(f"Font Family: {font.family()}")
+	font_path = gf.find_ttf_font(font.family())
+	if font_path:
+		print(f"Font path: {font_path}")
+	else:
+		print(f"Font '{font.family()}' not found in the fonts directory. ")
 
-    if font_path:
-        # Write on a blank image in the font
-        img = Image.new('RGB', (200, 100), color=(73, 109, 137))
-        d = ImageDraw.Draw(img)
-        d.text((10, 10), "Hello World", font=ImageFont.truetype(font_path, 20))
-        img.show()
+	if font_path:
+		# Write on a blank image in the font
+		img = Image.new('RGB', (200, 100), color=(73, 109, 137))
+		d = ImageDraw.Draw(img)
+		d.text((10, 10), "Hello World", font=ImageFont.truetype(font_path, 20))
+		img.show()
