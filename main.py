04d6b9c5f61ff5fcfc6cdcdd5876d0ab0f1de92f
Leon Gayle <103550846+Leospars@users.noreply.github.com>
2024-08-06 23:34:45 -0500

    Implement Creating ID Toolbox in main app

--- main.py
+++ main.py
@@ -8,12 +8,12 @@
 
 from PyQt5.QtCore import Qt, QRect
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QFileDialog, QApplication, QMainWindow
+from PyQt5.QtWidgets import QFileDialog, QApplication, QMainWindow, QDialog
 
 from generateID_ui import Ui_MainWindow
 from generateTabs import generateToolBox, updateToolBox
-from tests.SpecialComboBox import TtfComboBox
 from tests.canvas import Canvas
+from tests.windowBuild import centerRectOnScreen
 
 # from PyQt5.uic import loadUi
 
@@ -24,22 +24,35 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		super(QMainWindow, self).__init__()
 		# loadUi('generateID.ui', self)
 		self.setupUi(self)
+		self.show()
 		self.addWidgets()
 		self.setEventHandlers()
 
 	def addWidgets(self):
-		self.fontComboBox = TtfComboBox(self.fontComboBox)
-		self.canvas = Canvas(self.template_img.parent())
+		self.canvas = Canvas(self.frame)
 		self.canvas.setGeometry(self.template_img.geometry())
 		self.canvas.draw = lambda: self.addPaint(self.canvas)
-		generateToolBox(self, self.canvas)
+		print(f"canvas geometry: {self.canvas.geometry()} template_img geo {self.template_img.geometry()}\n"
+			  f"canvas hint {self.canvas.sizeHint()} template_img hint {self.template_img.sizeHint()}\n"
+			  f"canvas sizePolicy {self.canvas.sizePolicy()} template_img sizePolicy "
+			  f"{self.template_img.sizePolicy()}\n")
+
+		# create dialog window for adding data
+		self.dataDialog = QDialog()
+		self.dataDialog.setWindowTitle("Add Data For ID")
+		self.dataDialog.setGeometry(centerRectOnScreen(300, 400))
+
+		# remove all pages in tool box
+		for i in range(0, self.tool_box.count()):
+			self.tool_box.removeItem(i)
+		print(f"Toolbox is emptied now. Page Count: {self.tool_box.count()}")
+		generateToolBox(self.tool_box, self.canvas)
 
 	def setEventHandlers(self):
 		self.uploadButton.clicked.connect(self.uploadImage)
 		self.generateIDButton.clicked.connect(self.generateID)
 		self.addTextBoxButton.clicked.connect(self.addTextBox)
 		self.clearIDButton.clicked.connect(self.canvas.clearCanvas)
-		self.txtBox = Canvas(self)
 
 	def uploadImage(self):
 		template_filename, _ = QFileDialog.getOpenFileName(self, "Open Template File", "",
@@ -53,7 +66,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		# tint background
 		painter.setBrush(Qt.green)
 		painter.setOpacity(0.2)
-		painter.drawRect(self.geometry())
+		painter.drawRect(self.rect())
 
 	def generateID(self):
 		print("Generate ID")
@@ -70,6 +83,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		height = ev.pos().y() - y
 		font = self.fontComboBox.currentFont()  # get font from combobox
 
+		# get font size
+		font_size = self.fontSizeComboBox.currentText()
+		font.setPointSize(int(font_size))
+
 		# increment textBoxID
 		_id = self.idTypeName.text() if self.idTypeName.text() else self.idTypeName.placeholderText()
 		if _id in self.canvas.rectLabels:
@@ -81,14 +98,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 			_id=_id,
 			font=font
 		)
-		updateToolBox(self, self.canvas)
+		updateToolBox(self.tool_box, self.canvas)
 		self.canvas.unsetCursor()  # revert cursor
 		self.canvas.mousePressEvent = lambda ev: None  # remove event handler
 		print(f"{_id} added at {x, y} with size {width}x{height}")
+		print(f"Toolbox Page Count: {self.tool_box.count()}")
 
 	def createTextBoxOnClick(self, event):
-		# On first click get top  corner from user input
+		# On first click get top left corner from user input
 		x, y = event.pos().x(), event.pos().y()
+		# TODO: Show text box preview onMouseMove Event
 
 		# On second click
 		self.canvas.mousePressEvent = lambda ev: self.__addTextBox2ndClick__(ev, x, y)
