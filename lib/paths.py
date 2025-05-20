f298424ddae621b29641ebbdf208c9b177985961
leospars <leongayle018@gmail.com>
2025-05-20 10:05:51 -0500

    fix:Circular dependency on lib.configured_log

--- lib/paths.py
+++ lib/paths.py
@@ -1,21 +1,20 @@
 import os
 from pathlib import Path
-from lib.configured_log import log
 
-log(f"Paths: {Path(__file__).resolve()}")
+# print(f"Paths: {Path(__file__).resolve()}")
 def eval_base_dir(base_dir: Path) -> Path:
     if (base_dir / "_internal").exists() and (base_dir / "_internal").is_dir() :
         if base_dir.parent.parent  == "dist":
             base_dir = base_dir / "_internal"
             # This is a compiled distribution
-            log(f"Exe file: {Path(__file__)}")
-    else:
-        log(f"Running from python file: {__name__}")
+    #         print(f"Exe file: {Path(__file__)}")
+    # else:
+    #     print(f"Running from python file: {__name__}")
     return base_dir
 
 # ../.. => ./lib/paths or ./dist/_internal/lib/paths or ./dist/lib/paths
 BASE_DIR = eval_base_dir(Path(__file__).resolve().parent.parent)
-log(f"Base Dir: {BASE_DIR}")
+# print(f"Base Dir: {BASE_DIR}")
 
 FONT_DIR = BASE_DIR / "font"
 IMG_DIR = BASE_DIR / "img"
